c35d13127fe1d51404889824114ceba98bd220cf
LudmilaRos <ludmila.roslevica@gmail.com>
2022-11-09 21:40:30 +0200

    Task1

---
+++ src/test/java/Lecture7/SeleniumDemo.java
@@ -0,0 +1,33 @@
+package Lecture7;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumDemo {
+    @Test
+    public void someDemoTest() {
+        String path = "C:\\Users\\Ludmila\\IdeaProjects\\SomeProjectGradle\\src\\test\\resources\\chromedriver.exe";
+        System.setProperty("webDriver.chrome.driver", path);
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.ss.com/ru/");
+        driver.findElement(By.id("mtd_300")).click();
+        driver.findElement(By.id("f_o_8_min")).sendKeys("20");
+        driver.findElement(By.id("f_o_1276_max")).sendKeys("5");
+        driver.findElement(By.xpath("//input[@class='b s12']")).click();
+        Select dealType = new Select(driver.findElement(By.cssSelector("select[name='sid']")));
+        dealType.selectByValue("/ru/animals/dogs/sell/");
+
+//        Select dealType = new Select(driver.findElement(By.xpath("//select[@name='sid']")));
+
+
+
+
+
+        //  driver.quit();
+
+    }
+
+
+}
